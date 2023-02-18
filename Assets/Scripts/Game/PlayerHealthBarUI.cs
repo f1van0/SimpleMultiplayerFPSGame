cfd57d196711095ff165332e1c49e0db9a589ae4
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-18 16:38:47 +0300

    Prototyping Character

---
+++ Assets/Scripts/Game/PlayerHealthBarUI.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game
+{
+    public class PlayerHealthBarUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _healthLabel;
+
+        private int _maxHealth;
+        
+        public void Initialize(Character _character)
+        {
+            
+        }
+
+        private void SetHealth(int health)
+        {
+            _healthLabel.text = $"{health}/_maxHealth";
+        }
+    }
+}
