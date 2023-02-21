0de4ebf38c8e2067f438922628a64aa1d3c1d67a
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:32:39 +0300

    Added shooting component and projectile factory
    CharacterShootingController implements character shooting
    
    The projectile factory encapsulates the process of creating and initializing bullets

---
+++ Assets/Scripts/Game/Character/CharacterShootingController.cs
@@ -0,0 +1,52 @@
+﻿using JoyWay.Infrastructure.Factories;
+using JoyWay.Services;
+using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace JoyWay.Game.Character
+{
+    public class CharacterShootingController : NetworkBehaviour
+    {
+        [SerializeField] private Transform _handEndTransform;
+        
+        private PlayerInputs _playerInputs;
+        private CharacterLookController _lookController;
+        private ProjectileFactory _projectileFactory;
+
+        private Vector3 _lookDirection;
+
+        public void Initialize(PlayerInputs playerInputs, CharacterLookController lookController, ProjectileFactory projectileFactory)
+        {
+            if (isOwned)
+            {
+                _playerInputs = playerInputs;
+                _playerInputs.Character.Fire.performed += Fire;
+                _lookController = lookController;
+            }
+
+            if (isServer)
+            {
+                _projectileFactory = projectileFactory;
+            }
+        }
+
+        private void Fire(InputAction.CallbackContext obj)
+        {
+            _lookDirection = _lookController.GetLookDirection();
+            CmdFire(_handEndTransform.position, _lookDirection);
+        }
+
+        [Command]
+        private void CmdFire(Vector3 position, Vector3 lookDirection)
+        {
+            _projectileFactory.CreateFireball(position, lookDirection);
+        }
+
+        private void OnDestroy()
+        {
+            if (isOwned)
+                _playerInputs.Character.Fire.performed -= Fire;
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Factories/ProjectileFactory.cs
@@ -0,0 +1,26 @@
+﻿using JoyWay.Game.Projectiles;
+using JoyWay.Resources;
+using JoyWay.Services;
+using Mirror;
+using UnityEngine;
+
+namespace JoyWay.Infrastructure.Factories
+{
+    public class ProjectileFactory
+    {
+        private AssetContainer _assetContainer;
+        
+        public ProjectileFactory(AssetContainer assetContainer)
+        {
+            _assetContainer = assetContainer;
+        }
+        
+        public Projectile CreateFireball(Vector3 at, Vector3 direction)
+        {
+            Projectile fireball = Object.Instantiate(_assetContainer.Fireball.Value, at, Quaternion.identity);
+            NetworkServer.Spawn(fireball.gameObject);
+            fireball.Throw(direction);
+            return fireball;
+        }
+    }
+}
