841eca80c65a4f751a73cd943cdbaf2fa83acd1c
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:17:27 +0300

    Added CharacterHealth component
    This component will keep track of the character's health and inform you if it has been changed or if the character has died

---
+++ Assets/Scripts/Game/Character/CharacterHealth.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace JoyWay.Game.Character
+{
+    public class CharacterHealth : NetworkBehaviour
+    {
+        public Action<int> HealthChanged;
+        public Action<NetworkIdentity> Died;
+
+        [field: SerializeField] public int MaxHealth { get; private set; }
+        
+        [SyncVar(hook = nameof(SetHealth))]
+        private int _health;
+
+        public void Initialize()
+        {
+            if (!isServer)
+                return;
+            
+            _health = MaxHealth;
+            HealthChanged += x => CheckForDeath();
+        }
+        
+        [Server]
+        public void ApplyDamage(int damage)
+        {
+            _health -= damage;
+        }
+
+        [Server]
+        public void CheckForDeath()
+        {
+            if (_health < 0)
+            {
+                Died?.Invoke(this.netIdentity);
+            }
+        }
+
+        void SetHealth(int oldHealth, int newHealth)
+        {
+            HealthChanged?.Invoke(_health);
+        }
+    }
+}
