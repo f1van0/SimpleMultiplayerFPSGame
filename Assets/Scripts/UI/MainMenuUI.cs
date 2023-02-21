375ad626d133def447cde4d26296072013585bf4
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:56:32 +0300

    Prototyping MainMenuUI and removed Bootstrapper

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,9 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
 
-namespace UI
+namespace JoyWay.UI
 {
     public class MainMenuUI : MonoBehaviour
     {
+        public Button.ButtonClickedEvent Connect;
+        public Button.ButtonClickedEvent Host;
+        
+        [SerializeField] private GameObject _panel;
+        [SerializeField] private Button _hostButton;
+        [SerializeField] private Button _connectButton;
 
+        public void Initialize()
+        {
+            Host = _hostButton.onClick;
+            Connect = _connectButton.onClick;
+        }
+
+        public void Show()
+        {
+            _panel.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            _panel.SetActive(false);
+        }
     }
 }
