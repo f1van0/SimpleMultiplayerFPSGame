49a3ce083ccb1cfcbd75a8fd55a7cf709a9be7ab
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:20:28 +0300

    Added CharacterView and CharacterHealthBarUI
    This components will control the appearance and display information about the character

---
+++ Assets/Scripts/Game/Character/CharacterHealthBarUI.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace JoyWay.Game.Character
+{
+    public class CharacterHealthBarUI : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _healthLabel;
+
+        private int _maxHealth;
+        
+        public void Initialize(CharacterHealth characterHealth)
+        {
+            _maxHealth = characterHealth.MaxHealth;
+            SetHealth(_maxHealth);
+            characterHealth.HealthChanged += SetHealth;
+        }
+
+        private void SetHealth(int health)
+        {
+            _healthLabel.text = $"{health}/{_maxHealth}";
+        }
+    }
+}
