3e030a30fab09ac323b8ced95b79bbba1c9a587d
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:04:01 +0300

    Adde SceneLoader to load scenes asynchronously

---
+++ Assets/Scripts/Services/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace JoyWay.Services
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void Load(string name, Action onLoaded = null) => StartCoroutine((LoadScene(name, onLoaded)));
+
+        private IEnumerator LoadScene(string nextScene, Action onLoaded = null)
+        {
+            if (SceneManager.GetActiveScene().name == nextScene)
+            {
+                onLoaded?.Invoke();
+                yield break;
+            }
+
+            AsyncOperation waitNextScene = SceneManager.LoadSceneAsync(nextScene);
+
+            while (!waitNextScene.isDone)
+                yield return null;
+
+            onLoaded?.Invoke();
+        }
+    }
+}
