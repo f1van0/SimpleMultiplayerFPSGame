dd62ac5c36cfecd7e5d2ea4cb846c62eec0c174a
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:03:10 +0300

    Added Camera service to manage fps and main camera

---
+++ Assets/Scripts/Services/CameraService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Cinemachine;
+using Game;
+using UnityEngine;
+
+namespace JoyWay.Services
+{
+    public class CameraService : MonoBehaviour
+    {
+        [SerializeField]
+        private CinemachineVirtualCamera _fpsCamera;
+
+        private Camera _camera;
+
+        public Action<Vector3> LookDirectionUpdated;
+
+        public void Initialize()
+        {
+            _camera = Camera.main;
+        }
+
+        public void SetFollowTarget(Transform targetTransform)
+        {
+            _fpsCamera.Follow = targetTransform;
+        }
+
+        public Transform GetCameraTransform()
+        {
+            return _camera.transform;
+        }
+
+        private void LateUpdate()
+        {
+            LookDirectionUpdated?.Invoke(GetLookDirection());
+        }
+
+        public Vector3 GetLookDirection()
+        {
+            return _camera.transform.forward;
+        }
+    }
+}
