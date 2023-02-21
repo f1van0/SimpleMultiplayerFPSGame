895a6792fb0718f95cfcc1c292b93ecb7a367c2f
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:09:33 +0300

    Added Projectile with HitEffect scriptable object

---
+++ Assets/Scripts/Game/Projectiles/HitEffect.cs
@@ -0,0 +1,11 @@
+﻿using JoyWay.Game.Character;
+using UnityEngine;
+
+namespace JoyWay.Game.Projectiles
+{
+    [System.Serializable]
+    public abstract class HitEffect : ScriptableObject
+    {
+        public abstract void ApplyEffect(CharacterHealth characterHealth);
+    }
+}
---
+++ Assets/Scripts/Game/Projectiles/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using Game;
+using JoyWay.Game.Character;
+using Mirror;
+using UnityEngine;
+
+namespace JoyWay.Game.Projectiles
+{
+    public class Projectile : NetworkBehaviour
+    {
+        [SerializeField] protected Rigidbody _rigidbody;
+        [SerializeField] protected bool _handleCollision;
+        
+        [SerializeField] private HitEffect _hitEffect;
+        [SerializeField] private float _force;
+
+        [Server]
+        public virtual void Throw(Vector3 direction)
+        {
+            _rigidbody.AddForce(direction * _force, ForceMode.Impulse);
+        }
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!_handleCollision || !isServer)
+                return;
+            
+            if (other.gameObject.TryGetComponent<CharacterHealth>(out var characterHealth))
+            {
+                _hitEffect.ApplyEffect(characterHealth);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
