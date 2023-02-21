896e9eb5bf8279c6a1a4f0a44c2b9cc9fd17fffc
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:02:44 +0300

    Moved AssetContainer in Services

---
+++ Assets/Scripts/Services/AssetContainer.cs
@@ -0,0 +1,23 @@
+﻿using JoyWay.Game.Character;
+using JoyWay.Game.Projectiles;
+using JoyWay.Resources;
+using JoyWay.UI;
+using UnityEngine;
+
+namespace JoyWay.Services
+{
+    public class AssetContainer
+    {
+        public readonly LazyResource<CharacterContainer> Character =
+            new LazyResource<CharacterContainer>(Constants.Character);
+
+        public readonly LazyResource<Projectile> Fireball =
+            new LazyResource<Projectile>(Constants.Fireball);
+
+        public readonly LazyResource<MainMenuUI> MainMenuUI =
+            new LazyResource<MainMenuUI>(Constants.MainMenuUI);
+
+        public readonly LazyResource<GameObject> CrosshairUI =
+            new LazyResource<GameObject>(Constants.CrosshairUI);
+    }
+}
