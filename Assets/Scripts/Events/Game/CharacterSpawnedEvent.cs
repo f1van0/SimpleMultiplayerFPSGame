41b1ef94f7c2a78d6d368a0361c7a108d33644c4
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:45:10 +0300

    Added CharacterSpawnedEvent
    CharacterSpawnedEvent will be used to notify subscribers when a character has spawned.

---
+++ Assets/Scripts/Events/Game/CharacterSpawnedEvent.cs
@@ -0,0 +1,14 @@
+﻿using JoyWay.Game.Character;
+
+namespace Events.Game
+{
+    public class CharacterSpawnedEvent
+    {
+        public CharacterContainer CharacterContainer;
+
+        public CharacterSpawnedEvent(CharacterContainer characterContainer)
+        {
+            CharacterContainer = characterContainer;
+        }
+    }
+}
