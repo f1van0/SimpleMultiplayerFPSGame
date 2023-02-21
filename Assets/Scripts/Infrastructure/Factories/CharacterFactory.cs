1b13526cbe674129a5d30cf3ff164a1cc544038d
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:43:18 +0300

    Added CharacterFactory and UIFactory
    CharacterFactory will create and initialize characters
    UIFactory will create UI like MainMenuUI or CrossHairUI objects

---
+++ Assets/Scripts/Infrastructure/Factories/CharacterFactory.cs
@@ -0,0 +1,49 @@
+﻿using Cysharp.Threading.Tasks.Triggers;
+using Events.Game;
+using JoyWay.Game.Character;
+using JoyWay.Resources;
+using JoyWay.Services;
+using MessagePipe;
+using Mirror;
+using UnityEngine;
+using Zenject;
+
+namespace JoyWay.Infrastructure.Factories
+{
+    public class CharacterFactory
+    {
+        private PlayerInputs _playerInputs;
+        private CameraService _cameraService;
+        private AssetContainer _assetContainer;
+
+        private ISubscriber<CharacterSpawnedEvent> _subscriber;
+        private ProjectileFactory _projectileFactory;
+
+        public CharacterFactory(
+            PlayerInputs playerInputs,
+            CameraService cameraService,
+            AssetContainer assetContainer, 
+            ISubscriber<CharacterSpawnedEvent> subscriber,
+            ProjectileFactory projectileFactory)
+        {
+            _playerInputs = playerInputs;
+            _cameraService = cameraService;
+            _assetContainer = assetContainer;
+            _subscriber = subscriber;
+            _subscriber.Subscribe(x => InitializeSpawnedCharacter(x.CharacterContainer));
+            _projectileFactory = projectileFactory;
+        }
+
+        public CharacterContainer CreateCharacter(Transform at, NetworkConnectionToClient player)
+        {
+            CharacterContainer characterContainer = Object.Instantiate(_assetContainer.Character.Value, at.position, at.rotation);
+            NetworkServer.Spawn(characterContainer.gameObject, player);
+            return characterContainer;
+        }
+
+        private void InitializeSpawnedCharacter(CharacterContainer characterContainer)
+        {
+            characterContainer.Initialize(_playerInputs, _cameraService, _projectileFactory);
+        }
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Factories/UIFactory.cs
@@ -0,0 +1,26 @@
+﻿using JoyWay.Services;
+using JoyWay.UI;
+using UnityEngine;
+
+public class UIFactory
+{
+    private AssetContainer _assetContainer;
+
+    public UIFactory(AssetContainer assetContainer)
+    {
+        _assetContainer = assetContainer;
+    }
+    
+    public MainMenuUI CreateMainMenuUI()
+    {
+        MainMenuUI mainMenuUI = Object.Instantiate(_assetContainer.MainMenuUI.Value);
+        mainMenuUI.Initialize();
+        return mainMenuUI;
+    }
+
+    public GameObject CreateCrosshairUI()
+    {
+        GameObject crosshairUI = Object.Instantiate(_assetContainer.CrosshairUI.Value);
+        return crosshairUI;
+    }
+}
