a18170fd0202c874bbb599c5c7f779f2ad48285a
f1van <46835435+f1van0@users.noreply.github.com>
2023-02-21 20:36:56 +0300

    Added CharacterContainer and character prefab
    CharacterContainer will contain all the necessary components

---
+++ Assets/Scripts/Game/Character/CharacterContainer.cs
@@ -0,0 +1,45 @@
+﻿using JoyWay.Infrastructure;
+using JoyWay.Infrastructure.Factories;
+using JoyWay.Services;
+using UnityEngine;
+
+namespace JoyWay.Game.Character
+{
+    public class CharacterContainer : MonoBehaviour
+    {
+        [SerializeField] private CharacterHealth _characterHealth;
+        [SerializeField] private CharacterMovementController _movementController;
+        [SerializeField] private CharacterShootingController _shootingController;
+        [SerializeField] private CharacterInteractionController _interactionController;
+        [SerializeField] private CharacterLookController _lookController;
+        [SerializeField] private CharacterView _view;
+        [SerializeField] private Transform _eyesTransform;
+
+        public void Initialize(
+            PlayerInputs playerInputs,
+            CameraService cameraService,
+            ProjectileFactory projectileFactory)
+        {
+            //if (isServer)
+            //{
+                _characterHealth.Initialize();
+            //}
+            
+            //if (isLocalPlayer)
+            //{
+                _lookController.Initialize(cameraService);
+                _movementController.Initialize(playerInputs, _lookController);
+                _interactionController.Initialize(playerInputs, _lookController);
+            //}
+            
+            //TODO: change cameraService to conrtoller that can send information about camera rotation
+            _shootingController.Initialize(playerInputs, _lookController, projectileFactory);
+            _view.Initialize(_characterHealth, _lookController);
+        }
+
+        private void Start()
+        {
+            AdvancedNetworkManager.singleton.NotifyCharacterWasSpawned(this);
+        }
+    }
+}
